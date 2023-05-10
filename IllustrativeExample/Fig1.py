b335310bd9dfa7e45aace2f2fee85daf82122050
XIAOHEDEH <1037703142@qq.com>
2023-05-10 22:38:21 +0800

    Creat: Fig.1 python file, to plot Fig1

---
+++ IllustrativeExample/Fig1.py
@@ -0,0 +1,20 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+# # Fig.1(a): Polytope S1
+# x = np.array([0,2,1])
+# y = np.array([0,2,0])
+# plt.xlim(0,4)
+# plt.ylim(0,4)
+# plt.grid()
+# plt.plot(x,y,color='k')
+# plt.show()
+
+# # Fig.1(b): Polytope S2
+# x = np.array([0,1,1])
+# y = np.array([1,2,0])
+# plt.xlim(0,4)
+# plt.ylim(0,4)
+# plt.grid()
+# plt.plot(x,y,color='k')
+# plt.show()
